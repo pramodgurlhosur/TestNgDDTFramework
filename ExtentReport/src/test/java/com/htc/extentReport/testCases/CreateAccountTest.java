9de5ac17119d40603cd43adbbb436d773c95f71d
Pramod VG <gurlhosurpramod@gmail.com>
2021-02-23 18:50:12 +0530

    Extent Report implemented using ITestListener for the project

---
+++ ExtentReport/src/test/java/com/htc/extentReport/testCases/CreateAccountTest.java
@@ -0,0 +1,48 @@
+package com.htc.extentReport.testCases;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.htc.extentReport.base.TestBase;
+import com.htc.extentReport.pages.BasePage;
+import com.htc.extentReport.pages.CreateAccountPage;
+import com.htc.extentReport.pages.SignInPage;
+
+public class CreateAccountTest extends TestBase {
+	private WebDriver driver;
+	private SignInPage signInPage;
+	private BasePage basePage;
+	private CreateAccountPage createAccountPage;
+
+	public CreateAccountTest() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@BeforeClass
+	public void setUp() {
+		driver = getDriver();
+	}
+
+	@Test
+	public void verifyCreateAnAccountPage() {
+		System.out.println("Create An Account page test...");
+		basePage = new BasePage(driver);
+		signInPage = basePage.clickSignInBtn();
+		createAccountPage = signInPage.clickonCreateAnAccount();
+		Assert.assertTrue(createAccountPage.verifyPageTitle(), "Page title not matching");
+		Assert.assertTrue(createAccountPage.verifyCreateAccountPageText(), "Page text not matching");
+	}
+
+	@Test
+	public void createAccountExample1() {
+		System.out.println("Hey im in example1 test");
+	}
+
+	@Test
+	public void createAccountExample2() {
+		System.out.println("Hey im in Example2 test");
+	}
+}
