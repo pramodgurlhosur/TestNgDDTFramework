f956a6cc6079f04c624f5ef92dabf5d63b0ca57c
Pramod VG <gurlhosurpramod@gmail.com>
2021-05-27 22:21:31 +0530

    update

--- MadisonSeleniumPOM/src/test/java/com/htc/pages/LoginPage.java
+++ MadisonSeleniumPOM/src/test/java/com/htc/pages/LoginPage.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
 import com.htc.utilities.Util;
 
 public class LoginPage {
